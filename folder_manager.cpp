4d2727135eb0618e1e565dd4dbdca79d40ac597a
betterthanfaker <34050118+betterthanfaker@users.noreply.github.com>
2019-12-31 11:09:24 +0800

    newfolder
    new

--- folder_manager.cpp
+++ folder_manager.cpp
@@ -1,14 +1,33 @@
+<<<<<<< Updated upstream
 　#include "folder_manager.h"
+=======
+#include "folder_manager.h"
+#include "data_define.h"
+#include <queue>
+>>>>>>> Stashed changes
 
 FolderManager::FolderManager(DiskManager * dManager)
 {
         this->dManager = dManager;
+<<<<<<< Updated upstream
+=======
+        for(int i=0;i<128;i++){
+            fileTable[i].fileName = "";
+            fileTable[i].fileSize = 0;
+            fileTable[i].owner = "";
+            fileTable[i].type = 0;
+            fileLocks[i] = false;
+        }
+
+>>>>>>> Stashed changes
 }
+
 
 // 按照指针指向的FolderElement删除目录项
 int FolderManager::deleteData(FCB* element){
         // 删除磁盘中FCB
         this->dManager->receiveF_delete(element);
+<<<<<<< Updated upstream
         FCB* p;
         p=root->next;
         while(p!=nullptr){
@@ -25,10 +44,14 @@ int FolderManager::deleteData(FCB* element){
                 p=p->next;
             }
         }
+=======
+        return STATUS_OK;
+>>>>>>> Stashed changes
 }
 
 // 输出信息：获取所有文件
 queue<FCB*> FolderManager::getFiles(){
+<<<<<<< Updated upstream
         queue<FCB*> FCB_queue;
         FCB* p;
         p=root->next;
@@ -49,9 +72,55 @@ FCB* FolderManager::generateData(string data,string fileName){
         q->createTime=tm;
         q=p->next;
         return q;
+=======
+        queue<FCB*> q;
+        for(int i=0;i<128;i++){
+            if(fileTable[i].fileName != ""){
+                q.push(&fileTable[i]);
+            }
+            else continue;
+        }
+        return q;
+}
+
+// 创建新文件，注意：添加时记得往element里面填写tm时间数据
+int FolderManager::generateData(string data,string fileName){
+        // 获取时间
+        for(int i=0;i<128;i++){
+            if(fileTable[i].fileName == ""){
+                fileTable[i].fileName = fileName;
+                time_t t = time(0);
+                fileTable[i].createTime = localtime(&t);
+                this->dManager->receiveF_add(fileTable[i],fileName);
+                break;
+            }
+        }
+        return STATUS_OK;
+>>>>>>> Stashed changes
 }
 
 // 读取数据
 string FolderManager::getData(FCB * element){
+<<<<<<< Updated upstream
     return element->data;
+=======
+    string data_read = this->dManager->receiveF_read(element);
+    return data_read;
+}
+int FolderManager::lockFile(FCB * fcb){
+    for(int i=0;i<128;i++){
+        if(fcb->fileName == fileTable[i].fileName){
+            fileLocks[i] = true;
+        }
+    }
+    return STATUS_OK;
+}
+int FolderManager::unlockFile(FCB * fcb){
+    for(int i=0;i<128;i++){
+        if(fcb->fileName == fileTable[i].fileName){
+            fileLocks[i] = false;
+        }
+    }
+    return STATUS_OK;
+>>>>>>> Stashed changes
 }
--- folder_manager.h
+++ folder_manager.h
@@ -1,8 +1,8 @@
 #ifndef FOLDERMANAGER_H
 #define FOLDERMANAGER_H
-
-#include "data_define.h"
 #include "disk_manager.h"
+#include "data_define.h"
+#include <time.h>
 
 struct FCB{
     string name;
@@ -16,20 +16,29 @@ class FolderManager
 public:
     // 暂存磁盘管理的指针变量
     DiskManager * dManager;
-
-    FolderManager(DiskManager * dManager);
-
+    //FCN保存的
+    FCB fileTable[128];
+    bool fileLocks[128];
+    //初始化
+    FolderManager* fManager;(DiskManager * dManager);
     // 添加目录项，注意：添加时记得往element里面填写tm时间数据
-    FCB* generateData(string data,string fileName);
-
+    int generateData(string data,string fileName);
     // 按照指针指向的FolderElement删除目录项
     int deleteData(FCB* element);
-
     // 读取数据
     string getData(FCB* element);
+<<<<<<< Updated upstream
 
     // 输出信息：获取所有文件
     queue<FCB*> getFiles();
+=======
+    // 输出信息：获取所有文件
+    queue<FCB*> getFiles();
+    //上锁
+    int lockFile(FCB * fcb);
+    //不上锁
+    int unlockFile(FCB * fcb);
+>>>>>>> Stashed changes
 
 };
 
